2175037d8227aa76f17d4a995f6c47423afb28f3
Ricard Torres <torres.rick@gmail.com>
2022-07-13 10:43:13 +0200

    Change to fs-extra

--- build-css.js
+++ build-css.js
@@ -1,14 +1,3 @@
-import * as fs from 'fs';
+import fs from 'fs-extra';
 
-const origin = 'styles.css';
-const target = 'dist/styles.css';
-
-fs.copyFile(origin, target, (err) => {
-  if (err) {
-    console.log('An error occured while copying the folder.');
-    return console.error(err);
-  }
-  console.log('Copy completed!');
-
-  return true;
-});
+fs.copySync('./styles.css', './dist/styles.css')
